cfca344d8c5e55dab4f7058e41cabb4bc4729528
TORAN <toran.sahu@yahoo.com>
2017-12-18 02:24:58 +0530

    home, base, cs s,js added

--- toransahu-site/home/urls.py
+++ toransahu-site/home/urls.py
@@ -1,8 +1,12 @@
 """Home App Urls."""
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
+app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
