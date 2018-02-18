3e9950a01a160160d157686a87306ab55a905238
TORAN <toran.sahu@yahoo.com>
2018-02-19 00:17:56 +0530

    clustered settings files

---
+++ toransahu-site/toransahu/dev_settings.py
@@ -0,0 +1,42 @@
+from .base_settings import *
+
+
+# only required in development server, otherwise it will conflits with collectstatic in production
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+    '/var/www/static/',
+]
+
+
+# Production level settings - HTTPS may cause error in browser, use incognito tab
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
+SECURE_BROWSER_XSS_FILTER = False
+# X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = False
+
+
+SECURE_HSTS_SECONDS = 0
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_PRELOAD = False
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6r=p$2nh^@=)f_hpe@&dbm1h(yj^55^0wla^sm3*v2%#85p=jp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# allowed host not required in dev
+# ALLOWED_HOSTS = ['toransahu.pythonanywhere.com', '127.0.0.1']
+
+
+# not required to set STATIC_ROOT
+# STATIC_ROOT = os.path.join(BASE_DIR, 'allstatic') # don't name it 'static', during collection django gets confused
---
+++ toransahu-site/toransahu/prod_settings.py
@@ -0,0 +1,25 @@
+from .base_settings import *
+
+
+# Production level settings
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+SECURE_HSTS_SECONDS = 1
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6r=p$2nh^@=)f_hpe@&dbm1h(yj^55^0wla^sm3*v2%#85p=jp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['toransahu.pythonanywhere.com', '127.0.0.1']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'allstatic')
